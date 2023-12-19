988c79cef5cdd7173ca9e9e667591f872bdd11e5
Amit Gupta <amit_gupta@epam.com>
2023-12-19 18:56:17 +0530

    nc

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { db } from "@/db";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function editSnippet(id: number, code: string) {
@@ -8,6 +9,8 @@ export async function editSnippet(id: number, code: string) {
     where: { id },
     data: { code },
   });
+
+  revalidatePath(`/snippets/${id}`);
 
   redirect(`/snippets/${id}`);
 }
@@ -17,5 +20,48 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
+  revalidatePath("/");
+
+  redirect("/");
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3)
+      return {
+        message: "Title must be longer",
+      };
+
+    if (typeof code !== "string" || code.length < 10)
+      return {
+        message: "Code must be longer",
+      };
+
+    // Create a new record in the db
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error)
+      return {
+        message: err.message,
+      };
+
+    return {
+      message: "Something went wrong",
+    };
+  }
+
+  revalidatePath("/");
+  // Redirect the user back to root route
   redirect("/");
 }
